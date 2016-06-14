16335776dd462e9a47d133f2ef160561a9a5639b
Jose Eduardo Poma Caceres <jose.poma@outlook.com>
2016-06-14 01:05:05 -0500

    Emun

--- DemoASPNET4/Models/EmoEmotion.cs
+++ DemoASPNET4/Models/EmoEmotion.cs
@@ -12,6 +12,6 @@ namespace DemoASPNET4.Models
         public float Score { get; set; }
         public int EmoFaceId { get; set; }
         public EmoEmotionEnum EmotionType { get; set; }
-        public virtual EmoFace Face { get; set; }
+        public virtual EmoFace Face { get; set; } 
     }
 }
